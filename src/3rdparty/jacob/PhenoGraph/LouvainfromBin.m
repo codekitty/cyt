958cb7de766313f66a7a4aae968613f897ecf2c8
Jacob Levine <jl3545@columbia.edu>
2015-06-19 10:56:58 -0400

    Change warning to error and include tempfile cleanup before error is thrown

--- src/3rdparty/jacob/PhenoGraph/LouvainfromBin.m
+++ src/3rdparty/jacob/PhenoGraph/LouvainfromBin.m
@@ -16,32 +16,33 @@ for iter = 1:numiters
     fprintf(1,'MATLAB: running community detection, ITERATION %i\n', iter );
     command = [ps '/community ' filename '_graph.bin -l -1 -v -w ' filename '_graph.weights > ' filename '.tree'];
     fprintf( 1, '%s\n', command );
-    [s,r] = system( command );
+    [~,r] = system( command );
     fprintf(1, '\n');
     try
         % find each iteration's modularity
-        q = find_modularity( r )
+        q = find_modularity( r );
         fprintf( 1, 'MATLAB: modularity scores:\n' );
     catch
-        warning(sprintf('Unable to find modularity score in the stderr: %s.\nCheck that the correct path to the Louvain code is specified.', r));
+        cleanup(filename)
+        error('Unable to find modularity score in the stderr: %s.\nCheck that the correct path to the Louvain code is specified.', r);
     end
 
     
     % find number of lvevls
     command = [ps '/hierarchy ' filename '.tree' ];
     fprintf(1, '%s\n', command );
-    [s,r] = system( command );
+    [~,r] = system( command );
     fprintf(1, '\n' );
     
-    r = strtok(r,10);
+    r = strtok(r, 10);
     r = regexprep( r, 'Number of levels: ', '' );
-    num_levels = str2num( r )-1;
+    num_levels = str2double( r )-1;
     
     fprintf( 1, 'MATLAB: max level is %d\n', num_levels );
     
     % import levels
     for level = 1:num_levels
-        fprintf( 1, 'MATLAB: importing level %d\n', level )
+        fprintf( 1, 'MATLAB: importing level %d\n', level );
         command = [ps '/hierarchy ' filename '.tree -l ' num2str( level ) ' > ' filename '.tmp' ];
         fprintf(1, '%s\n', command );
         system( command );
@@ -61,17 +62,10 @@ for i = 1:numel(Q)
 end
 bestpartition = c{I,J};
 bestpartitionhierarchy = c(I,:);
-% clean up
-files{1} = dir([filename '*.tmp']);
-files{2} = dir([filename '*.tree']);
-files{3} = dir([filename '*.weights']);
-files{4} = dir([filename '*.bin']);
-for i = 1:4
-    for j = 1:length(files{i})
-        delete( files{i}(j).name )
-    end
-end
-%----------------------
+% delete temporary files
+cleanup(filename)
+
+%-------------------------
 function Q = find_modularity( r )
 % Q = find_modularity( r )
 % convert the text output into modularity score of each iteration
@@ -88,3 +82,15 @@ while( ~isempty( r ) )
         Q( idx ) = a( 2 );
     end
 end
+%-------------------------
+function cleanup(filename)
+files{1} = dir([filename '*.tmp']);
+files{2} = dir([filename '*.tree']);
+files{3} = dir([filename '*.weights']);
+files{4} = dir([filename '*.bin']);
+for i = 1:4
+    for j = 1:length(files{i})
+        delete( files{i}(j).name )
+    end
+end
+
